948e0d6ad05f8611b6116de24fa4653be2006fd1
Uriel Martinez <122043017@upq.edu.mx>
2025-05-21 17:36:32 -0600

    ejercicio a

--- practica2/a.js
+++ practica2/a.js
@@ -7,6 +7,8 @@ const persona = {
     }
 }
 
-// Aplica destructuracion aqui
+//desestructuración
+const { nombre, edad, direccion: { ciudad } } = persona;
 
-//imprime el mensaje
+//mensaje 
+console.log("Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.");
