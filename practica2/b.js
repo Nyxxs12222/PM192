3e210427bb87c6b335cc33bfabc8c1e986e2e1d8
Uriel Martinez <122043017@upq.edu.mx>
2025-05-21 17:09:54 -0600

    Estructura inicial de los ejercicios

---
+++ practica2/b.js
@@ -0,0 +1,10 @@
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "MOnitor", precio: 3000 }
+];
+
+//Tu codigo aqui 
+
+console.log(nombre); // ["Laptop", "Monitor"]
---
+++ practica2/c.js
@@ -0,0 +1,7 @@
+const persona = [
+    { nombre: "Ana, edad: 22"},
+    { nombre: "Luis, edad: 35"},
+    { nombre: "Maria, edad: 28"}
+]
+
+// Tu codigo aqui
