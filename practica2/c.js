40247d58c3d57c545eec119fd4e77ded48fd6c9d
Uriel Martinez <122043017@upq.edu.mx>
2025-05-22 19:00:10 -0600

    Practica 2 terminanda

--- practica2/c.js
+++ practica2/c.js
@@ -1,7 +1,16 @@
-const persona = [
-    { nombre: "Ana, edad: 22"},
-    { nombre: "Luis, edad: 35"},
-    { nombre: "Maria, edad: 28"}
-]
+const personas = [
+    { nombre: "Ana", edad: 22 },
+    { nombre: "Luis", edad: 35 },
+    { nombre: "Maria", edad: 28 }
+];
 
-// Tu codigo aqui
+const luis = personas.find(p => p.nombre === "Luis");
+
+personas.forEach(p => {
+    console.log(`${p.nombre} tiene ${p.edad} años`);
+});
+
+const totalEdades = personas.reduce((acum, p) => acum + p.edad, 0);
+
+console.log("Persona a encontrar:", luis);
+console.log("Suma total de edades:", totalEdades);
