ae4142d75d2455b3308913cac3a05b68c819bce8
Uriel Martinez <122043017@upq.edu.mx>
2025-05-23 09:08:53 -0600

    Ejercicio a

---
+++ practica 3/main.js
@@ -0,0 +1,5 @@
+import { restar } from './utils.js';
+
+console.log("Prueba 1: 10 - 5 =", restar(10, 5)); 
+console.log("Prueba 2: 3 - 7 =", restar(3, 7));  
+console.log("Prueba 3: 0 - 0 =", restar(0, 0));   
