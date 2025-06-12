3d5576f813c34a345efb67a58c3433148ebe68f8
Uriel Martinez <122043017@upq.edu.mx>
2025-06-11 18:32:56 -0600

    Objeto Propio Propiedad

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,29 @@
+/* Zone 1: Importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+/*Creamos un componente en base al componente base*/
+const Texto=(props)=>{
+  const {contenido}=props
+  return (
+    <Text>{contenido}</Text>
+  )
+}
+
+/* Zone 2: Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Buttom title="Press me!"></Buttom>
+
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Adios"></Texto>
     </View>
   );
 }
 
+/* Zone 3: Estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
